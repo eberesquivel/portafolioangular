6675d0abd180b625ccb25cdf26a2c14c3bfb35f3
Eber Raziel <eberesquivel@gmail.com>
2018-04-03 19:02:02 -0500

    rutas

---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,3 @@
+export { PortafolioComponent } from "./components/portafolio/portafolio.component.ts";
+export { AboutComponent } from "./components/about/about.component.ts";
+export { ProductosComponent } from "./components/productos/productos.component";
