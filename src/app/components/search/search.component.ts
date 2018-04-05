57f727ed233e2b943183f1f23fa314ae4de7003c
Eber Raziel <eberesquivel@gmail.com>
2018-04-04 19:50:27 -0500

    primera version

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ProductosService} from  "../../services/productos.service";
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent  {
 
-  constructor() { }
+termino:string = undefined;
 
-  ngOnInit() {
+  constructor(private route:ActivatedRoute,
+              private _ps: ProductosService) {
+      route.params.subscribe( parametros =>{
+          this.termino= parametros ['termino'];
+          // console.log(this.termino);
+
+          _ps.buscar_producto( this.termino)
+      });
+
   }
+
 
 }
